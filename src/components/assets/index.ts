93a7e9be4e4d5b2900b98f483e6ae74fa64b8cb4
강은빈 <kub0803@naver.com>
2021-08-01 21:13:47 +0900

    [ADD] assets 추가

---
+++ src/components/assets/index.ts
@@ -0,0 +1,7 @@
+export { default as blackgroundItem } from "backgroundItem.svg";
+export { default as blackBackground } from "blackBackground.svg";
+export { default as blackModeIcon } from "blackModeIcon.svg";
+export { default as whiteModeIcon } from "whiteModeIcon.svg";
+export { default as chatOutIcon } from "chatOutIcon.svg";
+export { default as imageIcon } from "imageIcon.svg";
+export { default as sendIcon } from "sendIcon.svg";
