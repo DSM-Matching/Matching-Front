0d9a3d7d6f26940190cef0dee8757fb1775fd663
강은빈 <kub0803@naver.com>
2021-08-01 23:15:22 +0900

    [MODIFY] assets 경로 수정

--- src/components/assets/index.ts
+++ src/components/assets/index.ts
@@ -1,7 +1,8 @@
-export { default as blackgroundItem } from "backgroundItem.svg";
-export { default as blackBackground } from "blackBackground.svg";
-export { default as blackModeIcon } from "blackModeIcon.svg";
-export { default as whiteModeIcon } from "whiteModeIcon.svg";
-export { default as chatOutIcon } from "chatOutIcon.svg";
-export { default as imageIcon } from "imageIcon.svg";
-export { default as sendIcon } from "sendIcon.svg";
+export { default as backgroundItem } from "./backgroundItem.svg";
+export { default as blackBackground } from "./blackBackground.svg";
+export { default as blackModeIcon } from "./blackModeIcon.svg";
+export { default as whiteModeIcon } from "./whiteModeIcon.svg";
+export { default as chatOutIcon } from "./chatOutIcon.svg";
+export { default as imageIcon } from "./imageIcon.svg";
+export { default as sendIcon } from "./sendIcon.svg";
+    
