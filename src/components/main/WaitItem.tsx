8921feaa1910c1e470271f471d6d775f5e4b90af
강은빈 <kub0803@naver.com>
2021-08-03 01:52:46 +0900

    [MODIFY]  대기 컴포넌트 UI 수정

--- src/components/main/WaitItem.tsx
+++ src/components/main/WaitItem.tsx
@@ -5,18 +5,22 @@ import {
   matchingButton,
   matchingLine,
   matchingLineCheck,
+  title,
 } from "../../utils/css/EmotionComponent";
 import { chating1 } from "../assets";
 
 const WaitItem = () => {
   return (
-    <div css={inputContainer}>
-      <img src={chating1} alt="채팅1" />
-      <div css={matchingLine}>
-        <div css={[matchingLine, matchingLineCheck]}></div>
+    <>
+      <h1 css={title}>Hi! 김지민님 안녕하세요.</h1>
+      <div css={inputContainer}>
+        <img src={chating1} alt="채팅1" />
+        <div css={matchingLine}>
+          <div css={[matchingLine, matchingLineCheck]}></div>
+        </div>
+        <button css={matchingButton}>Matching</button>
       </div>
-      <button css={matchingButton}>Matching</button>
-    </div>
+    </>
   );
 };
 
