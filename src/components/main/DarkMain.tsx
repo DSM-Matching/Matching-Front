8a3e92a3aa4fce5b336984a83bd50caf647ba10c
강은빈 <kub0803@naver.com>
2021-08-03 01:50:56 +0900

    [MODIFY] 다크모드 메인 수정

--- src/components/main/DarkMain.tsx
+++ src/components/main/DarkMain.tsx
@@ -1,19 +1,13 @@
 /** @jsxImportSource @emotion/react */
 
 import { useState } from "react";
-import {
-  inputBottomItem,
-  inputContainer,
-  inputTopItem,
-  mainWrapper,
-  modeContainer,
-  title,
-} from "../../utils/css/EmotionComponent";
+import { mainWrapper, modeContainer } from "../../utils/css/EmotionComponent";
 import { blackModeIcon, whiteModeIcon } from "../assets";
+import NickNameInput from "./NickNameInput";
 import WaitItem from "./WaitItem";
 
 const DarkMain = () => {
-  const [inputState, setInputState] = useState<boolean>(false);
+  const [changeComponent, setChangeComponent] = useState<boolean>(false);
 
   return (
     <>
@@ -22,28 +16,15 @@ const DarkMain = () => {
           <img src={whiteModeIcon} alt="화이트모드아이콘" />
           <img src={blackModeIcon} alt="다크모드아이콘" />
         </div>
-        <h1 css={title}>즐거운 Matching 시작</h1>
-        <WaitItem />
-        {/*  <div css={inputContainer}>
-          <div css={inputTopItem}>
-            <span>Please enter a nickname.</span>
-            <input
-              type="text"
-              placeholder="your nickname"
-              onChange={(e) =>
-                e.target.value.length === 0
-                  ? setInputState(false)
-                  : setInputState(true)
-              }
-            />
-          </div>
-          <div
-            css={[inputTopItem, inputBottomItem]}
-            style={{ boxShadow: inputState ? "0px 0px 29px #00b2ff" : "none" }}
-          >
-            join
-          </div>
-        </div> */}
+        {changeComponent ? (
+          <>
+            <WaitItem />
+          </>
+        ) : (
+          <>
+            <NickNameInput setChangeComponent={setChangeComponent} />
+          </>
+        )}
       </div>
     </>
   );
