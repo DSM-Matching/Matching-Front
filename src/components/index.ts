e66115ebb6e9551120cbd80409df67c66b277342
강은빈 <kub0803@naver.com>
2021-08-06 00:47:04 +0900

    [ADD] 컴포넌트 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export { default as DarkMain } from "./main/DarkMain";
 export { default as WaitItem } from "./main/WaitItem";
 export { default as Chatting } from "./chat/Chatting";
+export { default as ChatOutput } from "./chat/ChatOutput";
+export { default as ChatHeader } from "./chat/ChatHeader";
