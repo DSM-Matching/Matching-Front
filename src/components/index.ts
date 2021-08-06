42b56e9239b56e033d5406b4e76b4f9cd2c04405
강은빈 <kub0803@naver.com>
2021-08-06 23:48:16 +0900

    [ADD] 컴포넌트 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as WaitItem } from "./main/WaitItem";
 export { default as Chatting } from "./chat/Chatting";
 export { default as ChatOutput } from "./chat/ChatOutput";
 export { default as ChatHeader } from "./chat/ChatHeader";
+export { default as ChatInput } from "./chat/ChatInput";
