7faf7aa6d9d59ee2f675f1295f8358160709c55b
강은빈 <kub0803@naver.com>
2021-08-11 23:54:14 +0900

    [ADD] 채팅 떠나기 컴포넌트 추가

---
+++ src/utils/css/ModalEmotion.ts
@@ -0,0 +1,22 @@
+import { css } from "@emotion/react";
+
+export const flexColumn = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ModalBackground = css`
+  width: 100%;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.31);
+  position: absolute;
+`;
+
+export const ModalItem = css`
+  width: 400px;
+  height: 200px;
+  background: #ffffff;
+  box-shadow: 0px 0px 29px #00b2ff;
+`;
