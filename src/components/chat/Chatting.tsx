ff66ca7b499c78e4ff8f0be0a42e7295e67b3a58
강은빈 <kub0803@naver.com>
2021-08-11 23:55:56 +0900

    [MODIFY] 채팅 컴포넌트 코드 수정

--- src/components/chat/Chatting.tsx
+++ src/components/chat/Chatting.tsx
@@ -3,12 +3,13 @@
 import React from "react";
 
 import { chatContainer } from "../../utils/css/ChatEmotion";
-import { ChatHeader, ChatOutput, ChatInput } from "../index";
+import { ChatHeader, ChatOutput, ChatInput, ChatLeave } from "../index";
 
 const Chatting = () => {
   return (
     <>
       <section css={chatContainer}>
+        <ChatLeave />
         <ChatHeader />
         <ChatOutput />
         <ChatInput />
