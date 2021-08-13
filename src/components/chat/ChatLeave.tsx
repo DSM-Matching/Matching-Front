0c08462caa7d331c5089c71895ca031bd5857601
강은빈 <kub0803@naver.com>
2021-08-13 23:55:38 +0900

    [MODIFY] 채팅 나가기 모달

--- src/components/chat/ChatLeave.tsx
+++ src/components/chat/ChatLeave.tsx
@@ -1,24 +1,38 @@
 /** @jsxImportSource @emotion/react */
 
-import React from "react";
+import React, { useEffect } from "react";
+import { useRef } from "react";
+import { useState } from "react";
 import {
   ModalBackground,
   ModalItem,
   flexColumn,
   buttonContainer,
 } from "../../utils/css/ModalEmotion";
+import { modalOpenType } from "../../utils/interface/chatTypes";
 
-const ChatLeave = () => {
+const ChatLeave = ({ modalOpen, setModalOpen }: modalOpenType) => {
+  //const [modal, setModal] = useState<boolean>(false);
+  const test = useRef<boolean>(false);
+
+  const click = () => {
+    console.log(test.current);
+  };
+
   return (
     <>
-      <section css={[ModalBackground, flexColumn]}>
+      <section
+        style={{ display: modalOpen ? "flex" : "none" }}
+        css={[ModalBackground, flexColumn]}
+        onClick={click}
+      >
         <div css={[flexColumn, ModalItem]}>
           <div css={[flexColumn]}>
             <strong>김지민님</strong>
             <span>채팅 나가기를 하면 내용이 사라집니다.</span>
           </div>
           <div css={buttonContainer}>
-            <button>유지하기</button>
+            <button onClick={() => setModalOpen(false)}>유지하기</button>
             <button>채팅 나가기</button>
           </div>
         </div>
