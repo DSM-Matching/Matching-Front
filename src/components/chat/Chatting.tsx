624459f007d5bc3120e9438040debbb070bb4343
강은빈 <kub0803@naver.com>
2021-08-13 23:56:11 +0900

    [MODIFY] 채팅 컴포넌트 코드 수정

--- src/components/chat/Chatting.tsx
+++ src/components/chat/Chatting.tsx
@@ -1,16 +1,18 @@
 /** @jsxImportSource @emotion/react */
 
-import React from "react";
+import React, { useState } from "react";
 
 import { chatContainer } from "../../utils/css/ChatEmotion";
 import { ChatHeader, ChatOutput, ChatInput, ChatLeave } from "../index";
 
 const Chatting = () => {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+
   return (
     <>
       <section css={chatContainer}>
-        <ChatLeave />
-        <ChatHeader />
+        <ChatLeave modalOpen={modalOpen} setModalOpen={setModalOpen} />
+        <ChatHeader setModalOpen={setModalOpen} />
         <ChatOutput />
         <ChatInput />
       </section>
