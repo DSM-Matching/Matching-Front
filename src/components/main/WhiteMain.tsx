e94b0d31feecccf26b7aae9bac05914d3d062f20
Kimjimin0807 <jm004471@naver.com>
2021-08-19 17:15:07 +0900

    [ADD] 화이트 모드 메인 추가

--- src/components/main/WhiteMain.tsx
+++ src/components/main/WhiteMain.tsx
@@ -1,11 +1,16 @@
 /** @jsxImportSource @emotion/react */
 
 import React, { FC, useState } from "react";
+import {mainWrapper} from '../../utils/css/EmotionComponent';
 
 
 const WhiteMain = () => {
   return(
-    
+    <>
+      <div css={mainWrapper}>
+        
+      </div>
+    </>
   )
 };
 
