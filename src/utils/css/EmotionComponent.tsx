8daf2d0b59ae6566f6d1a8c706b37b2306f44515
Kimjimin0807 <jm004471@naver.com>
2021-08-19 17:14:56 +0900

    [ADD] css 컴포넌트

--- src/utils/css/EmotionComponent.tsx
+++ src/utils/css/EmotionComponent.tsx
@@ -1,2 +1,10 @@
 import { css } from '@emotion/react';
+import { whiteBackground } from '../../components/assets/index';
 
+export const mainWrapper = css`
+  width: 100%;
+  height: 100vh;
+  background-image: url(${whiteBackground});
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+`
