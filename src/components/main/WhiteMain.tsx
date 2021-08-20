a44657d6cf9664c14d6b7cd0dd5a1a8f1556518e
Kimjimin0807 <jm004471@naver.com>
2021-08-20 23:29:26 +0900

    [MODIFY] 화이트모드 메인 수정

--- src/components/main/WhiteMain.tsx
+++ src/components/main/WhiteMain.tsx
@@ -1,17 +1,25 @@
 /** @jsxImportSource @emotion/react */
 
 import React, { FC, useState } from "react";
-import {mainWrapper} from '../../utils/css/EmotionComponent';
-
+import { mainWrapper, modeContainer } from "../../utils/css/EmotionComponent";
+import { whiteModeDarkIcon, whiteModeWhiteIcon } from "../assets";
+import MatchWait from "./MatchWait";
+import NicknameInput from "./NicknameInput";
 
 const WhiteMain = () => {
-  return(
+  const [changeComponent, setChangeComponent] = useState<boolean>(true);
+
+  return (
     <>
       <div css={mainWrapper}>
-        
+        <div css={modeContainer}>
+          <img src={whiteModeWhiteIcon} alt="화이트모드"/>
+          <img src={whiteModeDarkIcon} alt="다크모드"/>
+        </div>
+        {changeComponent ? <MatchWait /> : <NicknameInput />}
       </div>
     </>
-  )
+  );
 };
 
 export default WhiteMain;
