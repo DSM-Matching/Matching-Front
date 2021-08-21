aaca0d4ebc5bd22b83db57ccc61bc0c83b01efa0
Kimjimin0807 <jm004471@naver.com>
2021-08-21 21:35:23 +0900

    [ADD] nickname input 추가

--- src/utils/css/EmotionComponent.tsx
+++ src/utils/css/EmotionComponent.tsx
@@ -20,11 +20,6 @@ export const modeContainer = css`
   }
 `;
 
-export const nicknameInputContainer = css`
-  width: 500px;
-  height: 200px;
-`;
-
 export const matchContainer = css`
   width: 500px;
   margin: auto;
@@ -66,3 +61,58 @@ export const matchButton = css`
   border-radius: 15px;
   cursor: pointer;
 `;
+
+export const inputContainer = css`
+  width: 500px;
+  margin: auto;
+  display: flex;
+  text-align: center;
+  flex-direction: column;
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+`;
+
+export const nicknameinput = css`
+  width: 500px;
+  height: 200px;
+  background: white;
+`;
+
+export const inputTopItem = css`
+  width: 100%;
+  height: 200px;
+  border: 1px solid #FFB2B2;
+  box-sizing: border-box;
+  border-radius: 5px;
+  box-shadow: 0px 0px 29px rgba(255, 178, 178, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  & span {
+    font-family: Segoe Script;
+    font-weight: bold;
+    font-size: 30px;
+    color: #FFB2B2;
+    margin-bottom: 20px;
+  }
+
+  & input{
+    width: 200px;
+    height: 40px;
+    font-family: Indie Flower;
+    font-size: 30px;
+    color: #FFB2B2;
+    text-align: center;
+
+    ::placeholder{
+      color: #FFB2B2;
+    }
+  }
+`;
+
+export const inputBottomItem = css`
+  
+`
