f416db7a0999865cbbd83cb0027787d72316a097
Kimjimin0807 <jm004471@naver.com>
2021-08-23 17:14:30 +0900

    [ADD] Chatting Header 추가

---
+++ src/components/chat/ChatHeader.tsx
@@ -0,0 +1,31 @@
+/** @jsxImportSource @emotion/react */
+
+import { dateList, header, modeChange } from "../../utils/css/ChattingEmotion";
+import {
+  whiteModeChatOutIcon,
+  whiteModeDarkIcon,
+  whiteModeWhiteIcon,
+} from "../assets";
+
+const ChatHeader = () => {
+  return (
+    <div css={header}>
+      <ul>
+        <li>
+          <img src={whiteModeChatOutIcon} alt="나가기 아이콘" />
+          <span>나가기</span>
+        </li>
+        <li css={dateList}>
+          <span>2021년 8월 22일</span>
+          <span>오후 09:54</span>
+        </li>
+        <li css={modeChange}>
+          <img src={whiteModeWhiteIcon} alt="화이트모드"/>
+          <img src={whiteModeDarkIcon} alt="다크모드"/>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default ChatHeader;
