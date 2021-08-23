90b2d7661dff5fb15b1b186e733d371288b0e1df
Kimjimin0807 <jm004471@naver.com>
2021-08-23 17:14:39 +0900

    [ADD] Chatting Input 추가

---
+++ src/components/chat/ChatInput.tsx
@@ -0,0 +1,27 @@
+/** @jsxImportSource @emotion/react */
+
+import React from "react";
+import { chatInput, chatInputContainer, inputItem } from "../../utils/css/ChattingEmotion";
+import { whiteChatSendIcon, whitePhotoUploadIcon } from "../assets";
+
+const ChatInput = () => {
+  return (
+    <>
+      <div css={chatInputContainer}>
+        <ul css={inputItem}>
+          <li> 
+            <img src={whitePhotoUploadIcon} alt="이미지 업로드 아이콘"/>
+          </li>
+          <li css={chatInput}>
+            <input />
+          </li>
+          <li>
+            <img src={whiteChatSendIcon} alt="채팅 전송 아이콘"/>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default ChatInput;
