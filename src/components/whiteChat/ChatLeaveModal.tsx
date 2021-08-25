223d4963ade6162c6ca2ef12729a2ca9e93057da
Kimjimin0807 <jm004471@naver.com>
2021-08-25 23:39:36 +0900

    [ADD] 모달 추가

--- src/components/whiteChat/ChatLeaveModal.tsx
+++ src/components/whiteChat/ChatLeaveModal.tsx
@@ -1,6 +1,7 @@
 /** @jsxImportSource @emotion/react */
 
 import React from "react";
+import { Link } from "react-router-dom";
 import {
   buttonContainer,
   chatLeaveContainer,
@@ -8,17 +9,24 @@ import {
   modalBackground,
 } from "../../utils/css/WhiteModalEmotion";
 
-const ChatLeaveModal = () => {
+const ChatLeaveModal = ({ setLeaveModal }: any) => {
   return (
     <>
       <div css={[modalBackground, flexcolum]}>
         <div css={[chatLeaveContainer, flexcolum]}>
           <b>김지민님</b>
           <span>채팅 나가기를 하면 내용이 사라집니다.</span>
-
           <div css={buttonContainer}>
-            <button>유지하기</button>
-            <button>나가기</button>
+            <button
+              onClick={() => {
+                setLeaveModal(false);
+              }}
+            >
+              유지하기
+            </button>
+            <Link to="/">
+              <button>나가기</button>
+            </Link>
           </div>
         </div>
       </div>
