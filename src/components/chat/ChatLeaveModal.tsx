60316cf80c53d9870e0dea32429e2a1077295af2
Kimjimin0807 <jm004471@naver.com>
2021-08-25 21:57:10 +0900

    [ADD] 채팅 나가기 모달 추가

--- src/components/chat/ChatLeaveModal.tsx
+++ src/components/chat/ChatLeaveModal.tsx
@@ -1,19 +1,29 @@
 /** @jsxImportSource @emotion/react */
 
 import React from "react";
-import { chatLeaveContainer, flexcolum, modalBackground } from "../../utils/css/ModalEmotion";
+import {
+  buttonContainer,
+  chatLeaveContainer,
+  flexcolum,
+  modalBackground,
+} from "../../utils/css/ModalEmotion";
 
 const ChatLeaveModal = () => {
-  return(
+  return (
     <>
       <div css={[modalBackground, flexcolum]}>
-        <div css={chatLeaveContainer}>
-          <span>김지민님</span>
+        <div css={[chatLeaveContainer, flexcolum]}>
+          <b>김지민님</b>
           <span>채팅 나가기를 하면 내용이 사라집니다.</span>
+
+          <div css={buttonContainer}>
+            <button>유지하기</button>
+            <button>나가기</button>
+          </div>
         </div>
       </div>
     </>
   );
-}
+};
 
 export default ChatLeaveModal;
--- src/utils/css/ModalEmotion.tsx
+++ src/utils/css/ModalEmotion.tsx
@@ -5,7 +5,7 @@ export const flexcolum = css`
   justify-content: center;
   align-items: center;
   flex-direction: column;
-`
+`;
 
 export const modalBackground = css`
   width: 100%;
@@ -13,12 +13,27 @@ export const modalBackground = css`
   background: rgba(255, 255, 255, 0.3);
   position: absolute;
   top: 0%;
-`
+`;
 
 export const chatLeaveContainer = css`
   width: 400px;
   height: 200px;
   background: white;
-  box-shadow: 0px 0px 29px #FED8F3;
-  text-align: center;
-`
+  box-shadow: 0px 0px 29px #fed8f3;
+`;
+
+export const buttonContainer = css`
+  margin-top: 30px;
+
+  & button {
+    width: 140px;
+    height: 40px;
+    margin: 15px;
+    cursor: pointer;
+    background: #ffe2f7;
+
+    :hover {
+      background: #ff9fe4;
+    }
+  }
+`;
