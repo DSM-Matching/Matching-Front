15b15aee0ecd4e8edf4bec6acef70f4b8e6f53bf
Kimjimin0807 <jm004471@naver.com>
2021-09-01 23:11:07 +0900

    [FEAT]  채팅 소켓 추가 및 전송 기능 추가

--- src/components/whiteChat/ChatInput.tsx
+++ src/components/whiteChat/ChatInput.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 
-import React from "react";
+import React, { useState } from "react";
 import {
   chatInput,
   chatInputContainer,
@@ -8,7 +8,15 @@ import {
 } from "../../utils/css/WhiteChattingEmotion";
 import { whiteChatSendIcon, whitePhotoUploadIcon } from "../assets";
 
-const ChatInput = () => {
+const ChatInput = ({ ws, setSendChatList, sendChatList }: any) => {
+  const [chatState, setChatState] = useState<string>("");
+
+  const sendChat = () => {
+    ws.send(chatState);
+    setSendChatList([...sendChatList, chatState]);
+    setChatState("");
+  };
+
   return (
     <>
       <div css={chatInputContainer}>
@@ -17,10 +25,19 @@ const ChatInput = () => {
             <img src={whitePhotoUploadIcon} alt="이미지 업로드 아이콘" />
           </li>
           <li css={chatInput}>
-            <input />
+              <input
+                onChange={(e) => {
+                  setChatState(e.target.value);
+                }}
+                value={chatState}
+              />
           </li>
           <li>
-            <img src={whiteChatSendIcon} alt="채팅 전송 아이콘" />
+            <img
+              src={whiteChatSendIcon}
+              alt="채팅 전송 아이콘"
+              onClick={sendChat}
+            />
           </li>
         </ul>
       </div>
