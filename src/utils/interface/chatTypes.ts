70b0c31e8d0a2ea5a4899e068f357c48d510c296
강은빈 <kub0803@naver.com>
2021-09-01 20:27:47 +0900

    [MODIFY]  채팅 타입 수정

--- src/utils/interface/chatTypes.ts
+++ src/utils/interface/chatTypes.ts
@@ -1,4 +1,11 @@
 export interface modalOpenType {
   setModalOpen?: any;
   modalOpen?: boolean;
+  socket?: any;
+  nickname?: string;
+}
+
+export interface chatInputProps {
+  setInput?: any;
+  socket?: any;
 }
