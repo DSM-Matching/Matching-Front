ec8897d9fd811f65d00b1838734dfa1ad570b21e
Kimjimin0807 <jm004471@naver.com>
2021-09-01 23:12:52 +0900

    [ADD] useHistory 추가 및 경로 설정

--- src/components/whiteMain/MatchWait.tsx
+++ src/components/whiteMain/MatchWait.tsx
@@ -1,16 +1,19 @@
 
 /** @jsxImportSource @emotion/react */
 
+import { useHistory } from 'react-router-dom';
 import { matchContainer, matchLoadingBar, matchButton } from '../../utils/css/WhiteEmotionComponent';
 import { matchingImg } from '../assets';
 
 const MatchWait = ({nickname} : any) => {
+  const history = useHistory();
+
   return(
     <div css={matchContainer}>
       <h1>Hi {nickname}님!</h1>
       <img src={matchingImg} alt="매칭이미지"/>
       <div css={[matchLoadingBar]}/>
-      <button css={matchButton}>Matching</button>
+      <button css={matchButton} onClick={() => history.push(`/chatting/${nickname}`)}>Matching</button>
     </div>
   )
 }
