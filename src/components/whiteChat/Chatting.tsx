50602ab63ce07a39e646d197e8158b8ede4708e6
Kimjimin0807 <jm004471@naver.com>
2021-09-01 23:12:21 +0900

    [ADD]  소켓 연동

--- src/components/whiteChat/Chatting.tsx
+++ src/components/whiteChat/Chatting.tsx
@@ -1,22 +1,43 @@
 /** @jsxImportSource @emotion/react */
 
-import React, { useState } from "react";
+import { WSAEACCES } from "constants";
+import React, { useEffect, useState } from "react";
 import { chattingContainer } from "../../utils/css/WhiteChattingEmotion";
 import ChatHeader from "./ChatHeader";
 import ChatInput from "./ChatInput";
 import ChatLeaveModal from "./ChatLeaveModal";
 import ChatOutput from "./ChatOutput";
 
-const Chatthing = () => {
+const Chatthing = ({ match }: any) => {
   const [leaveModal, setLeaveModal] = useState<boolean>(false);
+  const [sendChatList, setSendChatList] = useState<string[]>([]);
+  const [receptionChatList, setReceptionChatList] = useState<string[]>([]);
+  const [ws, setWs] = useState<any>();
+
+  useEffect(() => {
+    console.log("connect");
+    setWs(new WebSocket(`ws://172.20.10.6:8000/ws/${match.params.nickname}`));
+  }, [match.params.nickname]);
+
+  ws.onmessage = (e: any) => {
+    console.log(e);
+    setReceptionChatList([...receptionChatList, e.data]);
+  };
 
   return (
     <>
       <div css={chattingContainer}>
         <ChatHeader setLeaveModal={setLeaveModal} />
-        <ChatOutput />
-        <ChatInput />
-        {leaveModal && <ChatLeaveModal setLeaveModal={setLeaveModal} />}
+        <ChatOutput
+          sendChatList={sendChatList}
+          receptionChatList={receptionChatList}
+        />
+        <ChatInput
+          ws={ws}
+          setSendChatList={setSendChatList}
+          sendChatList={sendChatList}
+        />
+        {leaveModal && <ChatLeaveModal setLeaveModal={setLeaveModal} ws={ws} />}
       </div>
     </>
   );
